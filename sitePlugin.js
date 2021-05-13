493629fe4999ae9566b33576927f62321fe0aac6
Alasdair Smith <ali@alasdairsmith.co.uk>
2021-05-13 18:41:42 +0100

    Add a bunch of comments explaining how the trailing slash fix works

--- sitePlugin.js
+++ sitePlugin.js
@@ -12,29 +12,40 @@ const fs = require("fs-extra");
 // quick fix for GH pages trailing slash issues
 // /myDoc/index.html => /myDoc.html
 async function generateSimpleHtmlFiles(outDir) {
-  console.log("generateSimpleHtmlFiles", outDir);
+  // Docusaurus generates a index.html files based on the markdown file
+  // structure. For example, for the foo/bar/baz.md file, a
+  // foo/bar/baz/index.html file is generated.
 
+  // To workaround a problem with trailing slashes, we also copy the index.html
+  // file to another HTML file based on the markdown file name. For example, for
+  // the foo/bar/baz.md file, a foo/bar/baz.html file is copied from the
+  // foo/bar/baz/index.html (see above).
+
+  // Create a glob pattern to select all the generated index.html files
   const pattern = path.join(outDir, "/**/index.html");
-  // console.log('pattern', pattern);
 
-  const filePaths = (await glob(pattern)).filter((filePath) => {
+  // Get the file paths for all index.html files (ignoring a root index.html
+  // file)
+  const filePathsForIndexes = (await glob(pattern)).filter((filePath) => {
     return filePath !== path.join(outDir, "/index.html");
   });
 
-  // console.log('filePaths', filePaths);
-
   await Promise.all(
-    filePaths.map(async (filePath) => {
-      if ((await fs.stat(filePath)).isDirectory()) {
+    filePathsForIndexes.map(async (filePathForIndex) => {
+      if ((await fs.stat(filePathForIndex)).isDirectory()) {
         return;
       }
-      // console.log(file);
-      const filePathCopy = `${path.dirname(filePath)}.html`;
-      if (await fs.pathExists(filePathCopy)) {
-        // console.log(`Skipping ${filePathCopy}`);
+
+      // Make a HTML file path based on the directory name. For example, for the
+      // foo/bar directory, make a file path foo/bar.html
+      const filePathForDir = `${path.dirname(filePathForIndex)}.html`;
+
+      if (await fs.pathExists(filePathForDir)) {
+        // The file based on the directory name already exists, so skip
       } else {
-        await fs.copyFile(filePath, filePathCopy);
-        // console.log(`Created ${filePathCopy}`);
+        // Copy the file based on the index.html file to the file path based on
+        // the directory. For example, copy foo/bar/index.html to foo/bar.html
+        await fs.copyFile(filePathForIndex, filePathForDir);
       }
     })
   );
